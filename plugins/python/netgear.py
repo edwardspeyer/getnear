6dd43a08deb9e0f6b551678b45d760c97816df67
Edward Speyer <getnear@ed.wtf>
2019-06-11 13:03:59 +0000

    Neater Netgear library

--- plugins/python/netgear.py
+++ plugins/python/netgear.py
@@ -3,18 +3,57 @@ import os
 import re
 import urllib
 
+
+class PortTypes(object):
+    TAGGED = 'T'
+    UNTAGGED = 'U'
+
+
+class Config(object):
+    def __init__(self, dict):
+        self.dict = dict
+
+    def password(self):
+        return self.dict['password']
+
+    def ports(self):
+        for index, pc in enumerate(self.dict['vlan_ports']):
+            pvid = pc['pvid']
+            tagged = pc.get('tagged', [])
+            if not isinstance(tagged, list):
+                tagged = [tagged]
+            untagged = pc.get('untagged', [])
+            if not isinstance(untagged, list):
+                untagged = [untagged]
+            yield (index, pvid, tagged, untagged)
+
+    def vlan_ids(self):
+        all = [
+                [[pvid], tagged, untagged]
+                for (_, pvid, tagged, untagged) in self.ports()
+                ]
+        flat = [i for list1 in all for list2 in list1 for i in list2]
+        return sorted(set(flat))
+
+    def memberships(self):
+        for vlan_id in self.vlan_ids():
+            membership = [
+                    (
+                        PortTypes.TAGGED    if vlan_id in tagged else
+                        PortTypes.UNTAGGED  if vlan_id in untagged else
+                        None
+                    )
+                    for (_, pvid, tagged, untagged) in self.ports()
+                    ]
+            yield (vlan_id, membership)
+
+
 class Agent(object):
     def __init__(self, conn):
         self.conn = conn
         self.cookie = None
 
-    def get(self, path, params={}):
-        return self._request('GET', path, params)
-
-    def post(self, path, params={}):
-        return self._request('POST', path, params)
-
-    def _request(self, method, path, params={}):
+    def request(self, method, path, params={}):
         body = urllib.urlencode(params)
         headers = {}
         if self.cookie is not None:
@@ -27,289 +66,289 @@ class Agent(object):
         return response
 
 
-LOGIN = '/login.cgi'
-LOGOUT = '/logout.cgi'
-VLAN_CONFIG = '/8021qCf.cgi'
-VLAN_MEMBERS = '/8021qMembe.cgi'
-PORT_PVID = '/portPVID.cgi'
+class HTML(object):
+    RE_TAGS = re.compile(
+            """
+            < (\w+) (?: \s+ (.+?) )? /?>
+            """,
+            re.VERBOSE | re.DOTALL)
 
-class Actions(object):
+    RE_ATTRIBUTES = re.compile(
+            """
+            (\w+)
+            (?: = (?: (?: "(.*?)" ) | (?: '(.*?)' ) | (?: (.*?) ) ) )?
+            (?: \s | $)
+            """,
+            re.VERBOSE | re.DOTALL)
+
+
+    def __init__(self, html):
+        self.html = html
+
+    def __str__(self):
+        return self.html
+
+    def input_value(self, name):
+        for i in self.inputs():
+            if i.get('name') == name:
+                return i['value']
+
+    def inputs(self):
+        return self._tags_with_name('input')
+
+    def _tags_with_name(self, tag_name):
+        return [a for (t, a) in self._tags() if t == tag_name]
+
+    def _tags(self):
+        return [
+                (
+                    name,
+                    dict(
+                        (key, dq or sq or uq)
+                        for (key, dq, sq, uq)
+                        in re.findall(HTML.RE_ATTRIBUTES, attrs)
+                    )
+                )
+                for (name, attrs) in re.findall(HTML.RE_TAGS, self.html)
+                ]
+
+
+class Browser(object):
     def __init__(self, agent):
         self.agent = agent
+        self.page = None
+
+    def request(self, path, params={}):
+        method = 'GET'
+        if params:
+            method = 'POST'
+        #print((method, path, params))
+        response = self.agent.request(method, path, params)
+        self.page = HTML(response.read())
+        error = self.page.input_value('err_msg')
+        if error and (error != ''):
+            raise Exception(error)
+
+
+class MembershipCodec(object):
+    PORT_TYPES = {
+            '1': PortTypes.UNTAGGED,
+            '2': PortTypes.TAGGED,
+            '3': None,
+            }
+
+    PORT_CODES = dict((v, k) for k, v in PORT_TYPES.iteritems())
+
+    @classmethod
+    def decode(cls, enc):
+        return [cls.PORT_TYPES[c] for c in enc]
+
+    @classmethod
+    def encode(cls, membership):
+        return ''.join(cls.PORT_CODES[t] for t in membership)
+
+
+class Actions(object):
+    LOGIN = '/login.cgi'
+    LOGOUT = '/logout.cgi'
+    VLAN_CONFIG = '/8021qCf.cgi'
+    VLAN_MEMBERS = '/8021qMembe.cgi'
+    PORT_PVID = '/portPVID.cgi'
+
+    def __init__(self, browser):
+        self.browser = browser
 
     def login(self, password):
-        self.agent.post(LOGIN, {'password': password})
-    
+        self.browser.request(Actions.LOGIN, {'password': password})
+
     def logout(self):
-        self.agent.get(LOGOUT)
+        self.browser.request(Actions.LOGOUT)
 
-    def is_advanced_VLAN_enabled(self):
-        body = self.get_advanced_VLAN_status()
-        for input in inputs(body):
-            if (input['name'] == 'status') and ('checked' in input):
-                return input['value'] == 'Enable'
-        raise Exception("Could not find a checked radio button")
+    def is_vlans_enabled(self):
+        self.browser.request(Actions.VLAN_CONFIG)
+        for i in self.browser.page.inputs():
+            if i.get('name') == 'status' and 'checked' in i:
+                return i['value'] == 'Enable'
+        raise Exception("couldn't tell if vlans enabled!")
 
-    def enable_advanced_VLAN(self):
-        body = self.get_advanced_VLAN_status()
-        hash = self.get_input(body, 'hash')
-        self.agent.post(VLAN_CONFIG, {'status': 'Enable', 'hash': hash})
-
-    def get_advanced_VLAN_status(self):
-        return self.agent.get(VLAN_CONFIG).read()
+    def enable_vlans(self):
+        self.browser.request(Actions.VLAN_CONFIG)
+        self.browser.request(
+                Actions.VLAN_CONFIG,
+                {
+                    'status': 'Enable',
+                    'hash': self.browser.page.input_value('hash')
+                })
 
     def get_vlans(self):
-        body = self.get_advanced_VLAN_status()
+        self.browser.request(Actions.VLAN_CONFIG)
         vlans = [
-                int(input['value'])
-                for input in inputs(body)
-                if input['name'].startswith('vlanck')
+                int(i['value'])
+                for i in self.browser.page.inputs()
+                if i['name'].startswith('vlanck')
                 ]
         return sorted(vlans)
 
     def add_vlan(self, vlan_id):
-        current_vlans = self.get_vlans()
-        if vlan_id in current_vlans:
-            return False
-        index = len(current_vlans)
-        body = self.get_advanced_VLAN_status()
-        hash = self.get_input(body, 'hash')
+        self.browser.request(Actions.VLAN_CONFIG)
         params = {
-                'status': 'Enable',
-                'ADD_VLANID': str(vlan_id),
-                'vlanNum': len(current_vlans),
-                'hash': hash,
-                'ACTION': 'Add'
+                'status':       'Enable',
+                'ADD_VLANID':   str(vlan_id),
+                'vlanNum':      self.browser.page.input_value('vlanNum'),
+                'hash':         self.browser.page.input_value('hash'),
+                'ACTION':       'Add'
                 }
-        self.agent.post(VLAN_CONFIG, params)
-        return True
+        self.browser.request(Actions.VLAN_CONFIG, params)
 
     def delete_vlan(self, vlan_id):
-        body = self.agent.get(VLAN_CONFIG).read()
-        hash = self.get_input(body, 'hash')
+        self.browser.request(Actions.VLAN_CONFIG)
         vlanck = next(
                 i.get('name')
-                for i in inputs(body)
+                for i in self.browser.page.inputs()
                 if i.get('name', '').startswith('vlanck')
                 and i.get('value') == str(vlan_id)
-                )
-        vlan_num = next(
-                i.get('value')
-                for i in inputs(body)
-                if i.get('name') == 'vlanNum'
                 )
         params = {
                 'status':       'Enable',
                 'ADD_VLANID':   '',
-                vlanck:         str(vlan_id),
-                'vlanNum':      vlan_num,
-                'hash':         hash,
+                vlanck:         vlan_id,
+                'vlanNum':      self.browser.page.input_value('vlanNum'),
+                'hash':         self.browser.page.input_value('hash'),
                 'ACTION':       'Delete',
                 }
-        self.agent.post(VLAN_CONFIG, params)
+        self.browser.request(Actions.VLAN_CONFIG, params)
 
-    def get_members(self, vlan_id):
-        return self.get_or_set_members(vlan_id, None)
-
-    def set_members(self, vlan_id, port_types):
-        return self.get_or_set_members(vlan_id, port_types)
-
-    def get_or_set_members(self, vlan_id, port_types):
-        # Going to VLAN_MEMBERS returns a form for changing the VLAN with the
-        # lowest number.
-        #
-        # Submit that form without changing anything to get the form we want.
-        body = self.agent.get(VLAN_MEMBERS).read()
-        hash = self.get_input(body, 'hash')
-        encoded_membership = self.get_input(body, 'hiddenMem')
-        option_re = '<option value="(\d+)"( selected)?>'
-        current_vlan_id = next(
-            int(id)
-            for id, sel in re.findall(option_re, body)
-            if sel is not ''
-            )
+    def get_membership(self, vlan_id):
+        """
+        To get the members of a vlan we first have to submit members for the
+        first VLAN while simultaneously asking for the members of the vlan we
+        care about...
+        """
+        self.browser.request(Actions.VLAN_MEMBERS)
         params = {
-                'VLAN_ID': vlan_id,
-                'hash': hash,
-                'hiddenMem': encoded_membership,
+                'VLAN_ID':      vlan_id,
+                'hash':         self.browser.page.input_value('hash'),
+                'hiddenMem':    self.browser.page.input_value('hiddenMem'),
                 }
+        self.browser.request(Actions.VLAN_MEMBERS, params)
+        return MembershipCodec.decode(
+                self.browser.page.input_value('hiddenMem'))
 
-        body = self.agent.post(VLAN_MEMBERS, params).read()
-        encoded_membership = self.get_input(body, 'hiddenMem')
-        current_port_types = decode_membership(encoded_membership)
-
-        if port_types is None:
-            return current_port_types
-
-        if current_port_types == port_types:
-            return False
-
-        hash = self.get_input(body, 'hash')
-        encoded_membership = encode_membership(port_types)
+    def set_membership(self, vlan_id, membership):
+        self.get_membership(vlan_id)
         params = {
-                'VLAN_ID': vlan_id,
-                'hash': hash,
-                'hiddenMem': encoded_membership,
+                'VLAN_ID':      vlan_id,
+                'hash':         self.browser.page.input_value('hash'),
+                'hiddenMem':    MembershipCodec.encode(membership),
                 }
-        self.agent.post(VLAN_MEMBERS, params)
-        return True
+        self.browser.request(Actions.VLAN_MEMBERS, params)
 
-    def get_input(self, body, name):
-        return next(
-                i['value']
-                for i in inputs(body)
-                if ('name' in i) and (i['name'] == name)
-                )
-
-    def get_pvids(self):
-        body = self.agent.get(PORT_PVID).read()
+    def get_pvid(self, port_index):
+        self.browser.request(Actions.PORT_PVID)
+        html = self.browser.page.html
         pvids_re = '<td class="def" sel="input">(\d+)'
-        return [int(s) for s in re.findall(pvids_re, body)]
+        pvids = [int(s) for s in re.findall(pvids_re, html)]
+        return pvids[port_index]
 
-    def set_pvid(self, port_number, pvid):
-        body = self.agent.get(PORT_PVID).read()
-        hash = self.get_input(body, 'hash')
-        port_key = 'port{}'.format(port_number)
+    def set_pvid(self, port_index, pvid):
+        self.browser.request(Actions.PORT_PVID)
+        #body = self.agent.get(PORT_PVID).read()
+        #hash = self.get_input(body, 'hash')
+        port_key = 'port{}'.format(port_index)
         params = {
-                'pvid': str(pvid),
-                port_key: 'checked',
-                'hash': hash,
+                'pvid':     str(pvid),
+                port_key:   'checked',
+                'hash':     self.browser.page.input_value('hash'),
                 }
-        body = self.agent.post(PORT_PVID, params).read()
-        self.raise_errors(body)
+        self.browser.request(Actions.PORT_PVID, params)
 
-    def raise_errors(self, body):
-        error_re = "id='err_msg' value='(.+?)'"
-        match = re.search(error_re, body)
-        if match:
-            raise Exception(match.groups()[0])
 
-def inputs(html):
-    result = {}
-    matches = re.findall("<input(\s+.+?)>", html)
-    return [
-            dict(re.findall("\s+(\w+)(?:=['\"]?(\w+)['\"]?)?", match))
-            for match in matches
-            ]
-
-# 31312 = _U_UT
-PORT_UNTAGGED = 'U'
-PORT_TAGGED = 'T'
-PORT_NOT_A_MEMBER = '_'
-
-PORT_TYPES = {
-        '1': PORT_UNTAGGED,
-        '2': PORT_TAGGED,
-        '3': PORT_NOT_A_MEMBER,
-        }
-
-PORT_CODES = dict((v, k) for k, v in PORT_TYPES.iteritems())
-
-def decode_membership(encoded_membership):
-    return [PORT_TYPES[c] for c in encoded_membership]
-
-def encode_membership(port_types):
-    return ''.join(
-            PORT_CODES[t] for t in port_types
-            )
+import yaml
+config = Config(yaml.load(
+        """
+        password: password
+        vlan_ports:
+            - pvid: 1
+              tagged: [12, 13, 14, 15]
+              untagged: [1]
+            - pvid: 12
+              untagged: [12]
+            - pvid: 13
+              untagged: [13]
+            - pvid: 14
+              untagged: [14]
+            - pvid: 1
+              tagged: [12, 13, 14, 15]
+              untagged: [1]
+        """))
 
 fqdn = '10.3.1.50'
 password = 'password'
 conn = httplib.HTTPConnection(fqdn, 80)
 agent = Agent(conn)
-actions = Actions(agent)
-
-import yaml
-ports_config = yaml.load(
-        """
-        - pvid: 1
-          tagged: [1, 12, 13, 14, 15]
-        - pvid: 12
-          untagged: 12
-        - pvid: 74
-          untagged: 74
-          tagged: [12, 15]
-        - pvid: 99
-          untagged: 99
-        - pvid: 1
-          tagged: [1, 12, 13, 14, 15]
-        """)
-
-class Config(object):
-    def __init__(self, ports_config):
-        self.ports_config = ports_config
-    
-    def ports(self):
-        for pc in self.ports_config:
-            pvid = pc['pvid']
-            tagged = pc.get('tagged', [])
-            if not isinstance(tagged, list):
-                tagged = [tagged]
-            untagged = pc.get('untagged', [])
-            if not isinstance(untagged, list):
-                untagged = [untagged]
-            yield (pvid, tagged, untagged)
-
-    def vlan_ids(self):
-        all = [
-                [[pvid], tagged, untagged]
-                for (pvid, tagged, untagged) in self.ports()
-                ]
-        flat = [i for list1 in all for list2 in list1 for i in list2]
-        return sorted(set(flat))
-
-    def memberships(self):
-        for vlan_id in self.vlan_ids():
-            membership = [
-                    (
-                        PORT_TAGGED     if vlan_id in tagged else
-                        PORT_UNTAGGED   if vlan_id in untagged else
-                        PORT_NOT_A_MEMBER
-                    )
-                    for (pvid, tagged, untagged) in self.ports()
-                    ]
-            yield (vlan_id, membership)
-
-
-config = Config(ports_config)
+browser = Browser(agent)
+actions = Actions(browser)
 
 try:
-    actions.login(password)
-    if not actions.is_advanced_VLAN_enabled():
-        print "ENABLE_VLAN"
-        actions.enable_advanced_VLAN()
-
     did_change = False
+    actions.login(password)
 
-    # Sync config to switch:
-    for (vlan_id, membership) in config.memberships():
-        print("ADD VLAN {}".format(vlan_id))
-        if actions.add_vlan(vlan_id):
+    if not actions.is_vlans_enabled():
+        did_change = True
+        print "Enabling VLANs"
+        actions.enable_vlans()
+
+    current_vlans = actions.get_vlans()
+
+    # Add all VLANs
+    for vlan_id in config.vlan_ids():
+        if vlan_id not in current_vlans:
+            print "Adding new VLAN {}".format(vlan_id)
+            actions.add_vlan(vlan_id)
             did_change = True
 
-        print("VLAN MEMBERSHIP {!r}".format(membership))
-        if actions.set_members(vlan_id, membership):
+    for (index, pvid_vlan, _, _) in config.ports():
+        # Ensure port is untagged member of its PVID VLAN
+        membership = actions.get_membership(pvid_vlan)
+        if membership[index] != PortTypes.UNTAGGED:
+            print(
+                    "Changing port #{} to be untagged member of VLAN {}".format(
+                        index + 1, pvid_vlan))
+            membership[index] = PortTypes.UNTAGGED
+            actions.set_membership(pvid_vlan, membership)
             did_change = True
 
-    for (port_index, (pvid, _, _)) in enumerate(config.ports()):
-        print("PORT {} PVID {}".format(port_index, pvid))
-        if actions.set_pvid(port_index, pvid):
+        # Set PVID
+        if actions.get_pvid(index) != pvid_vlan:
+            print(
+                    "Setting PVID for port #{} to {}".format(
+                        index + 1, pvid_vlan))
+            actions.set_pvid(index, pvid_vlan)
+            did_change = True
+
+    # Set membership
+    for vlan_id, membership in config.memberships():
+        if actions.get_membership(vlan_id) != membership:
+            print(
+                    "Changing VLAN {:4d} membership: {!r}".format(
+                        vlan_id,
+                        membership))
+            actions.set_membership(vlan_id, membership)
             did_change = True
 
     # Delete unwanted config from switch:
-    for vlan_id in actions.get_vlans():
+    for vlan_id in current_vlans:
         if vlan_id in config.vlan_ids():
             continue
-        
-        print("DELETE MEMBERSHIP {}".format(vlan_id))
-        null_membership = [PORT_NOT_A_MEMBER for _ in config.ports()]
-        if actions.set_members(vlan_id, null_membership):
-            did_change = True
+        did_change = True
 
-        print("DELETE VLAN {}".format(vlan_id))
-        if actions.delete_vlan(vlan_id):
-            did_change = True
+        print("Delet membership for VLAN {}".format(vlan_id))
+        null_membership = [None for _ in config.ports()]
+        actions.set_membership(vlan_id, null_membership)
+
+        print("Delete VLAN {}".format(vlan_id))
+        actions.delete_vlan(vlan_id)
+
 
 finally:
-    print "Logging out..."
     actions.logout()
