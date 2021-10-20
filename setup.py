99b18e4f79ddce57e535c5f7248784f6cbf863f3
Edward Speyer <getnear@ed.wtf>
2021-10-20 11:20:55 +0100

    Adding setup.py to install this directly

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+        name='getnear',
+        version='20020902',
+        author='Edward Speyer',
+        author_email='getnear@ed.wtf',
+        packages=['getnear'])
