59cb68ec8cbc7546f68b08a8198a634ae0fa5d07
Edward Speyer <getnear@ed.wtf>
2021-10-20 12:02:55 +0100

    Dependency: the requests HTTP library

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import setuptools
 
 setuptools.setup(
         name='getnear',
-        version='20020902',
+        version='20020903',
         author='Edward Speyer',
         author_email='getnear@ed.wtf',
+        install_requires=['requests'],
         packages=['getnear'])
