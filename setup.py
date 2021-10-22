27b7b4f66d20b6a52b105f5b295ccae3a8587178
Edward Speyer <edwardspeyer@users.noreply.github.com>
2021-10-22 14:03:07 +0100

    Add tabulate as a dependency

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import setuptools
 
 setuptools.setup(
         name='getnear',
-        version='20020905',
+        version='20020906',
         author='Edward Speyer',
         author_email='getnear@ed.wtf',
-        install_requires=['requests'],
+        install_requires=['requests', 'tabulate'],
         packages=['getnear'],
         scripts=['scripts/getnear'])
