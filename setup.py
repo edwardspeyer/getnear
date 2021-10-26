ed1650d9105685b84b9f6934bf9920720cd6443c
EGS <edwardspeyer@users.noreply.github.com>
2021-10-26 12:12:20 +0100

    version++

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name='getnear',
-        version='20020907',
+        version='20020908',
         author='Edward Speyer',
         author_email='getnear@ed.wtf',
         install_requires=['requests', 'tabulate'],
